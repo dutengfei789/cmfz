2ebe0f95e73489d0ac5f16b0e23b415f58bd6a57
du <dutengfei789@126.com>
2019-02-19 14:27:59 +0800

    使用springboot搭建ssm环境

---
+++ springboot-ssm/src/main/java/com/baizhi/springbootssm/com/baizhi/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.baizhi.springbootssm.com.baizhi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("test")
+    public String test(){
+        return "hello world";
+    }
+}
